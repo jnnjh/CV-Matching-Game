342bb15c6b3f5f1a19a844b934f6d479c3a15577
aliciawt <aliciawynonatjahjadi@gmail.com>
2026-07-01 16:15:31 +0200

    register statement routes in app

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import { userRoutes } from './routes/users.js'
 import { gameRoutes } from './routes/games.js'
 import { playerRoutes } from './routes/players.js'
 import { lobbyRoutes } from './routes/lobby.js'
+import { statementRoutes } from './routes/statements.js'
 
 export const app = express()
 
